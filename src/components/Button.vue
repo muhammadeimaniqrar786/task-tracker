24d61c4ff00d8eb6dcaad98ad6e000c007fc5d73
Muhammad Eiman Iqrar <muhammadeimaniqrar786@gmail.com>
2021-11-24 10:31:15 -0800

    Stable Code with Delete Task Functionality

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">{{ title }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        title: {
+            type: String,
+            default: 'button text',
+        },
+        color: {
+            type: String,
+            default: 'Black',
+        },
+    },
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
+}
+</script>
