4ccf4cdb82aca547a0944a4cf6ee3ff6e86d2fa3
MuhammadEimanIqrar <70460539+MuhammadEimanIqrar@users.noreply.github.com>
2021-12-19 23:08:18 -0800

    Add Task Toggle Functionality Working Done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
-    <AddTask @add-task="addTask" />
-    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <Header title="Task Tracker" @toggle-add-task="toggleAddTask" :showAddTask="showAddTask"/>
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Tasks,
@@ -21,55 +27,61 @@ export default {
   data() {
     return {
       tasks: [],
-    }
+      showAddTask: false,
+    };
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
     addTask(task) {
-      this.tasks = [...this.tasks, task]
+      this.tasks = [...this.tasks, task];
     },
     deleteTask(id) {
-      if (confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+      if (confirm("Are you sure?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
       }
     },
     toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task );
-    }
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
       {
         id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
         reminder: true,
       },
       {
         id: 2,
-        text: 'Meeting at School',
-        day: 'March 3rd at 1:30pm',
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
         reminder: true,
       },
       {
         id: 3,
-        text: 'Food Shopping',
-        day: 'March 3rd at 11:00am',
+        text: "Food Shopping",
+        day: "March 3rd at 11:00am",
         reminder: false,
       },
-    ]
-  }
-}
+    ];
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,37 @@
 <template>
-    <header>
-        <h1>{{ title }}</h1>
-        <Button title="Add Task" color="green" />
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :title="showAddTask ? 'close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
 </template>
 
 <script>
-import Button from './Button'
+import Button from "./Button";
 
 export default {
-    name: 'Header',
-    props: {
-        title: {
-            type: String,
-            default: "Title"
-        }
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Title",
     },
-    components: {
-        Button
-    }
-}
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+};
 </script>
 
 <style scoped>
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 </style>
